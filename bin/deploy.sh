7d7083ea2ec624d071f0475ec86f3abb63bcc312
Kyle TechSquidTV <git@techsquidtv.com>
2023-05-28 03:33:58 +0000

    feat!: new deploy and upgrade scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-source ./bin/loadenv.sh
-
-if [ -z "$1" ]; then
-    echo "Error: No depoyment specified"
-    exit 1
-fi
 
 echo "The deployment script is only meant to be ran once. If you need to upgrade the deployment, use bin/upgrade.sh instead."
 echo "Are you sure you want to continue? (y/n)"
 read -r response
-if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+if [[ ! ${response,,} =~ ^y(es)?$ ]]; then
     exit 0
 fi
 
-helm install "$1" ./ -f values.yaml -f <(envsubst < secrets.yaml)
+helm install "$1" ./ -f values.yaml -f secrets.yaml
--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-source ./bin/loadenv.sh
-
-if [ -z "$1" ]; then
-    echo "Error: No depoyment specified"
-    exit 1
-fi
-
-helm upgrade -f values.yaml -f <(envsubst < secrets.yaml) "$1" .
+helm upgrade -f values.yaml -f secrets.yaml  "$1" .
